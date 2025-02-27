6c074f3e124fd88547ed839eda41103160b45b4d
suryansh22s <suryanshs@anuvrittiksha.com>
2025-02-28 04:52:18 +0530

    frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react";
 import "./index.css"; // Ensure TailwindCSS styles are applied
-import Dashboard from "./components/Dashboard"; // Import your main component
+import Navbar from "./components/Navbar";
+import Dashboard from "./components/Dashboard";
+import AddTransaction from "./components/addTransaction";
+import CSVUploader from "./components/CSVUploader";
+import FinanceChart from "./components/FinanceChart";
+import FinancialGoal from "./components/FinancialGoal";
+import Login from "./components/Login";
+import Reminder from "./components/Reminder";
+import TransactionList from "./components/TransactionList";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-      <Dashboard />
+    <div className="min-h-screen bg-gray-100 flex flex-col">
+      <Navbar />
+      <div className="flex flex-1 p-4">
+        <main className="flex-1">
+          <Dashboard />
+          <AddTransaction />
+          <TransactionList />
+          <FinanceChart />
+          <FinancialGoal />
+          <CSVUploader />
+          <Reminder />
+        </main>
+      </div>
+      <Login />
     </div>
   );
 }
